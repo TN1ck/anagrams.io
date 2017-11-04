17ba379a88397b7bec2f57cd2f15eadfd3d88a69
TN1ck <tomwanick@gmail.com>
2017-11-04 12:27:00 +0100

    chore: first steps for having tests

---
+++ src/__tests__/anagram.ts
@@ -0,0 +1,24 @@
+import * as anagram from '../anagram';
+
+// I use this one, because I tested it, and it doesn't have so many solutions and takes
+// some time to calculate
+const TEST_WORD = 'johannesvonplato';
+const TEST_WORD_SOLUTIONS = [
+  'loan jet pan oh on vs',
+  'loan jet pan no oh vs',
+  'plan jet oh on on vs a',
+  'plan jet no oh on vs a',
+  'plan jet no no oh vs a',
+  'jet lap ah on on on vs',
+  'jet lap ah no on on vs',
+  'jet lap ah no no on vs',
+  'jet lap ah no no no vs',
+];
+const NUMBER_OF_SUBANAGRAMS = 112;
+
+describe('Anagram / findanagrams', () => {
+  test('Should find the the anagrams', () => {
+    const {subanagrams, generator} = anagram.findAnagramSentences(TEST_WORD, anagram.dictionaries.engUS1);
+    expect(subanagrams.length).toBe(NUMBER_OF_SUBANAGRAMS);
+  });
+});
