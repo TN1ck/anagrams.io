932fe8b1e34aa8fbb6ed1bd42f2b00c617a8d2f8
TN1ck <tomwanick@gmail.com>
2017-11-05 21:23:20 +0100

    fix: backend build scripts

--- ts-backend/src/__tests__/anagram.ts
+++ ts-backend/src/__tests__/anagram.ts
@@ -4,23 +4,23 @@ import dictionaries from 'src/dictionaries';
 // I use this one, because I tested it, and it doesn't have so many solutions and takes
 // some time to calculate
 const TEST_WORD = 'johannesvonplato';
-const TEST_WORD_SOLUTIONS = [
-  'loan jet pan oh on vs',
-  'loan jet pan no oh vs',
-  'plan jet oh on on vs a',
-  'plan jet no oh on vs a',
-  'plan jet no no oh vs a',
-  'jet lap ah on on on vs',
-  'jet lap ah no on on vs',
-  'jet lap ah no no on vs',
-  'jet lap ah no no no vs',
-];
+// const TEST_WORD_SOLUTIONS = [
+//   'loan jet pan oh on vs',
+//   'loan jet pan no oh vs',
+//   'plan jet oh on on vs a',
+//   'plan jet no oh on vs a',
+//   'plan jet no no oh vs a',
+//   'jet lap ah on on on vs',
+//   'jet lap ah no on on vs',
+//   'jet lap ah no no on vs',
+//   'jet lap ah no no no vs',
+// ];
 const NUMBER_OF_SUBANAGRAMS = 112;
 
 describe('Anagram / findanagrams', () => {
   test('Should find the the anagrams', () => {
     const subanagrams = anagram.findSortedSubAnagrmns(TEST_WORD, dictionaries.engUS1);
     expect(subanagrams.length).toBe(NUMBER_OF_SUBANAGRAMS);
-    const {generator} = anagram.findAnagramSentences(TEST_WORD, subanagrams);
+    // const {generator} = anagram.findAnagramSentences(TEST_WORD, subanagrams);
   });
 });
--- ts-backend/src/dictionaries.ts
+++ ts-backend/src/dictionaries.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import {parseDictionary} from 'src/../../ts-frontend/src/dictionaries';
+import {parseDictionary} from '../../ts-frontend/src/dictionaries';
 
 function readFile(path: string) {
   return fs.readFileSync(path).toString();
@@ -8,7 +8,7 @@ function readFile(path: string) {
 
 const dictionaries = {
   // TOOD: fix this
-  engUS1: parseDictionary(readFile(path.join(__dirname, '../../dictionaries/eng-us-1.csv'))),
-  engUS2: parseDictionary(readFile(path.join(__dirname, '../../dictionaries/eng-us-2.csv'))),
+  engUS1: parseDictionary(readFile(path.join(__dirname, '../../../../dictionaries/eng-us-1.csv'))),
+  engUS2: parseDictionary(readFile(path.join(__dirname, '../../../../dictionaries/eng-us-2.csv'))),
 }
 export default dictionaries;
