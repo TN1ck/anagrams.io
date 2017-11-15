5bc4d246013d3d3d9977b126604ba28c4438b72c
TN1ck <tomwanick@gmail.com>
2017-11-15 23:13:46 +0100

    feat: add frontend deploy script

---
+++ ts-frontend/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf dist
+npm run build
+tar -zcvf dist.tar.gz dist
+scp ./dist.tar.gz tom@anagramania.io:/tmp/dist.tar.gz
+ssh tom@anagramania.io 'cd /tmp && tar -zxvf dist.tar.gz && rm -rf /opt/apps/anagramania/ts-frontend/dist && mv ./dist /opt/apps/anagramania/ts-frontend/'
