4442714af6eda6acd7a2a101988235c9238e100a
TN1ck <tomwanick@gmail.com>
2017-11-19 02:12:58 +0100

    feat: try to disable zooming in ios

--- ts-frontend/src/index.tsx
+++ ts-frontend/src/index.tsx
@@ -3,6 +3,30 @@ import * as ReactDom from 'react-dom';
 
 import Root from './pages/Root';
 
+//
+// https://stackoverflow.com/questions/37808180/disable-viewport-zooming-ios-10-safari
+// disable zooming on ios 10.3+
+//
+
+document.addEventListener('touchmove', function(event: any) {
+  if (event.scale !== 1) {
+     event.preventDefault();
+  }
+}, false);
+
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
+//
+// render the app
+//
+
 function renderApp(Component) {
   ReactDom.render(
     <Component />,
