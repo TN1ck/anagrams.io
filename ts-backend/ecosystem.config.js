4d0493b27e572c736d84e40af73d8c7bf21545c4
Tom Nick <tomwanick@gmail.com>
2020-03-01 13:28:03 +0100

    fix: wrong path

--- ts-backend/ecosystem.config.js
+++ ts-backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "anagrams-backend",
-      script: "./dist/ts-backend/index.js",
+      script: "./dist/ts-backend/src/index.js",
       node_args: "--max_old_space_size=8192"
     }
   ]
