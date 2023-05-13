15f82b7155b8798db57fb41664f359d956d3d0ec
Tom Nick <tomwanick@gmail.com>
2023-05-14 01:14:58 +0200

    chore: small type improvements

--- ts-frontend/src/constants.ts
+++ ts-frontend/src/constants.ts
@@ -1,4 +1,3 @@
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-export const BACKEND_URL = IS_PRODUCTION ? 'https://api.anagrams.io' : 'http://localhost:3000';
 export const FRONTEND_URL = IS_PRODUCTION ? 'https://anagrams.io' : 'http://localhost:3001';
